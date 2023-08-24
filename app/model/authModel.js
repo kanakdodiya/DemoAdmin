bdf2c9e6ac71b76db1991b2bfe8ae445f7f853d6
vishvjeet <vishvjeet.rathod@isyncevolution.com>
2023-08-24 17:03:08 +0530

    [fix] user modal

--- app/model/authModel.js
+++ app/model/authModel.js
@@ -23,6 +23,6 @@ const userSchema = new mongoose.Schema({
     timestamps: true, // Enable timestamps
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('users', userSchema);
 
 module.exports = User;
